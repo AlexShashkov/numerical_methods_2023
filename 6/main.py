9d889043e86e60c45e0f54350a680a8f72c17bac
Alexander Shashkov <45598701+AlexShashkov@users.noreply.github.com>
2023-10-24 09:48:31 +0300

    Add files via upload

--- 6/main.py
+++ 6/main.py
@@ -1,71 +1,61 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-def findMax(matrix):
-    """
-    Нахождения максимамального по модулю элемента aij
-    j > i
-    """
-    aij = [0] * n
-    for i in range(n):
-        for j in range(n):
-            if j > i and np.abs(aij[0]) < np.abs(matrix[i][j]):
-                aij = [matrix[i][j], i, j]
-    return aij
+def p(x):
+    return -4 * (x**2 + 3) / (x * (x**2 + 6))
 
+def q(x):
+    return 6 / (x**2 + 6)
 
-def phi(matrix, a, i, j):
-    """
-    Функция нахождения sin и cos угла поворота
-    a - max
-    """
-    phi = 0.5 * np.arctan(2 * a / (matrix[i][i] - matrix[j][j]))
-    return phi
+def fin_diff(y_a, y_b, a, b, h):
+    n = int((b - a) / h)
+    x_values = np.linspace(a, b, n+1)
+    A = np.zeros((n+1, n+1))
+    B = np.zeros(n+1)
+    A[0, 0] = 1.0
+    A[0, 1] = -1.0
+    A[n, n] = h-1
+    A[n, n-1] = 1.0
+    B[0] = -y_a*h
+    B[n] = y_b*h
+    for i in range(1, n):
+        A[i, i] = -2.0 + q(x_values[i])*np.power(h, 2)
+        A[i, i+1] = 1.0 + 0.5*p(x_values[i])*h
+        A[i, i-1] = 1.0 - 0.5*p(x_values[i])*h
+    y_values = np.linalg.solve(A, B)
+    return x_values, y_values
 
+def finite_difference_derivative(x, y, h):
+    n = len(x)
+    derivative = [(y[i+1] - y[i-1]) / (2 * h) if i > 0 and i < n-1 else 0 for i in range(n)]
+    return derivative
 
-# Метод вращения Якоби
-def jacoby(matrix, debug = False):
-    _max = findMax(matrix)[0]
-    x = np.eye(n)
-    k = 0
-    while np.abs(_max) > eps:
-        print("Итерация", k)
-        _max, i, j = findMax(matrix)
-        print('max|aij| = ', _max)
-        ph = phi(matrix, _max, i, j)
+x0 = 0
+xn = 4
+ya = 0
+yb = 26
+h1 = 0.1
+h2 = 0.01
+x_res_h1, y_res_h1 = fin_diff(ya, yb, x0, xn, h1)
+x_res_h2, y_res_h2 = fin_diff(ya, yb, x0, xn, h2)
 
-        sin, cos = np.sin(ph), np.cos(ph)
-        print(f" Sin и cos угла поворота: sin(phi_{k})={sin}", f"cos(phi_{k})={cos}")
-        U = np.eye(n)
-        U[i][i] = U[j][j] = cos
-        U[i][j] = -sin
-        U[j][i] = sin
-        print('U =', U)
-        U_T = U.transpose()
-        print('Transpose of U =', U_T)
-        x = np.matmul(x, U)
-        matrix = np.matmul(np.matmul(U_T, matrix), U)
-        print(f"A({k+1})=U({k})^T*A({k})*U({k}) = ", matrix)
-        k += 1
-    for j in range(n):
-        x[:, j] /= x[j][j]
-    return matrix.diagonal(), x
+print("y1", y_res_h1)
+print("y2", y_res_h2)
 
-eps = 0.01
-A = np.array([[-8.0, 5.0, -7.0],
-              [5.0, 1.0, 4.0],
-              [-7.0, 4.0, 4.0]])
-n = len(A)
+# Вычисляем производные
+derivative_h1 = finite_difference_derivative(x_res_h1, y_res_h1, h1)
+derivative_h2 = finite_difference_derivative(x_res_h2, y_res_h2, h2)
 
-print('Исходная матрица', A)
-M, X = jacoby(A)
-print('Собственные значения:')
-for i in range(n):
-    print(f'lamda_{i + 1} = ', M[i])
-print('Найденные собственные вектора:')
-for j in range(n):
-    print(f"X{j + 1} =", X[:, j])
-print('Проверка:')
-print(np.linalg.eigh(A)[0])
-vector = np.linalg.eigh(A)[1]/np.linalg.eigh(A)[1].diagonal()
-for j in range(n):
-    print(f'X{j + 1} = ', vector[:, j])
+plt
+
+# print('В нуле:', derivative_h2[0])
+
+# print(f'Разность в точке {x_res_h1[-1]} = {y_res_h2[-1] - derivative_h2[-2]}')
+
+plt.title('Конечно-разностный метод')
+plt.plot(x_res_h1, y_res_h1, label='y(x) при h = 0.1')
+plt.plot(x_res_h2, y_res_h2, label='y(x) при h = 0.01')
+plt.plot(x_res_h1[:-1], derivative_h1[:-1], label="y'(x)")
+plt.legend()
+plt.grid(True)
+plt.show()
